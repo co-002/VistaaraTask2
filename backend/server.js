504fa092dcd3057b8ed59d09e8104e3c56bd37e5
co-002 <aniketypandit99@gmail.com>
2025-04-03 17:58:00 +0530

    fully backend part is completed

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./DB_connection.js";
-import customerRouter from "./routes/customer.js"
-import transactionRouter from "./routes/transaction.js"
+import customerRouter from "./routes/customer.js";
+import cors from "cors";
+import transactionRouter from "./routes/transaction.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  })
+);
 
 // Database connection method
 connectDB();
 
-app.use("/api", customerRouter)
-app.use("/api", transactionRouter)
+app.use("/api", customerRouter);
+app.use("/api", transactionRouter);
 
 app.listen(port, () => {
-    console.log("Server is running on port: ", port)
-})
+  console.log("Server is running on port: ", port);
+});
